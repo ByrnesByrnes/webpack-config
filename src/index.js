d57eefc8237e05e4fb5985b5c2b638042504be9b
Myron <myronbyrnes@gmail.com>
2021-03-03 10:22:38 -0500

    add babel to project

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ const life = {
   strive: "Greatness Nigga",
   glory: "Go for GOLD only!!"
 }
+const iCode = {
+  code: "I can CODE",
+  coder: "I can CODE even better"
+}
+
 
 console.log(life)
+console.log(iCode)
